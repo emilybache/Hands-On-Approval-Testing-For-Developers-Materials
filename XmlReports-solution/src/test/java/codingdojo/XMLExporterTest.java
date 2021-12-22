671aae0f3caa4007a29b6f3951e3f72ddca2061c
emily <emily@bacheconsulting.com>
2021-12-22 10:21:46 +0100

    improve sample solution

--- XmlReports-solution/src/test/java/codingdojo/XMLExporterTest.java
+++ XmlReports-solution/src/test/java/codingdojo/XMLExporterTest.java
@@ -6,42 +6,43 @@ import org.approvaltests.core.Options;
 import org.approvaltests.scrubbers.RegExScrubber;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 
 import static codingdojo.SampleModelObjects.*;
 
 public class XMLExporterTest {
 
     @Test
-    public void exportFull() throws Exception {
-        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
+    public void exportFull() {
+        var orders = List.of(RecentOrder, OldOrder);
         String xml = XMLExporter.exportFull(orders);
         Approvals.verifyXml(xml);
     }
 
     @Test
-    public void exportTaxDetails() throws Exception {
-        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
+    public void exportTaxDetails() {
+        var orders = List.of(RecentOrder, OldOrder);
         String xml = XMLExporter.exportTaxDetails(orders);
         Approvals.verifyXml(xml);
     }
 
     @Test
-    public void exportStore() throws Exception {
+    public void exportStore() {
         Store store = FlagshipStore;
         String xml = XMLExporter.exportStore(store);
         Approvals.verifyXml(xml);
     }
 
     @Test
-    public void exportHistory() throws Exception {
-        Collection<Order> orders = Arrays.asList(RecentOrder, OldOrder);
+    public void exportHistory() {
+        var orders = List.of(RecentOrder, OldOrder);
         String xml = XMLExporter.exportHistory(orders);
-        String regex = "createdAt='[^']+'";
-        Approvals.verifyXml(xml, new Options(new RegExScrubber(
-                "createdAt=\"[^\"]+\"",
-                "createdAt=\"2018-09-20T00:00Z\"")));
+        Approvals.verifyXml(xml,
+                new Options(new RegExScrubber(
+                        "createdAt=\"[^\"]+\"",
+                        "createdAt=\"2018-09-20T00:00Z\"")
+                )
+        );
     }
 
 }
